51b79e122fa90b129b599bae8dfc1692c29fe464
Lizy <LizyZZZ@163.com>
2023-11-23 22:30:17 +0800

    add proto test

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module go-grpc-demo
 
 go 1.20
 
-require (
-	github.com/golang/protobuf v1.5.3 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
-)
+require google.golang.org/protobuf v1.31.0
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"go-grpc-demo/service"
+
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	user := &service.User{
+		Username: "lizy",
+		Age:      25,
+	}
+
+	data, err := proto.Marshal(user)
+	if err != nil {
+		panic(err)
+	}
+
+	userRecv := &service.User{}
+
+	if err = proto.Unmarshal(data, userRecv); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(user.String())
+
+}
